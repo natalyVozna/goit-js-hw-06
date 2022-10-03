00c094c4788dfa62e3e71c39b12fb769ecc60df2
natalyVozna <109243941+natalyVozna@users.noreply.github.com>
2022-10-03 15:26:33 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -9,12 +9,11 @@ const ingredients = [
 
 const ulEl = document.querySelector("#ingredients");
 
-const containerLi = [];
-ingredients.map((el) => {
+const containerLi = ingredients.map((el) => {
   const liEl = document.createElement("li");
   liEl.textContent = el;
   liEl.classList.add("item");
-  containerLi.push(liEl);
+  return liEl;
 });
 
 ulEl.append(...containerLi);
